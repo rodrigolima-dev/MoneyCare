c7aa490d5f50bab6fdf5da8fde943b351aad719b
rodrigolima-dev <rodrigolima.developer@gmail.com>
2024-06-30 15:11:05 -0300

    safe-commit

--- src/routes/app.routes.js
+++ src/routes/app.routes.js
@@ -1,17 +1,55 @@
+import 'react-native-gesture-handler';
 import React from "react";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import Home from "../pages/Home";
+import Registry from '../pages/Registry';
+import Profile from '../pages/Profile';
+import Ionicons from 'react-native-vector-icons/Ionicons'
 
 
-const AppStack = createNativeStackNavigator();
+
+const AppTab = createBottomTabNavigator();
 
 export default function AppRoutes () {
     return(
-        <AppStack.Navigator>
-            <AppStack.Screen
-            name="Home"
+        <AppTab.Navigator
+        screenOptions={({route}) => ({
+            tabBarIcon: ({focused, color, size}) => {
+                let iconName;
+
+                if (route.name === 'Home') {
+                    iconName = focused
+                    ? 'home'
+                    : 'home-outline'
+                } else if (route.name === 'Registry') {
+                    iconName = focused
+                    ? 'duplicate'
+                    : 'duplicate-outline'
+                } else if(route.name === 'Profile') {
+                    iconName = focused
+                    ? 'person'
+                    : 'person-outline'
+                }
+
+                return <Ionicons name={iconName} size={size} color={color}/>
+            }
+        })}
+        >
+
+            <AppTab.Screen
+            name='Home'
             component={Home}
             />
-        </AppStack.Navigator>
+
+            <AppTab.Screen
+            name='Registry'
+            component={Registry}
+            />
+
+            <AppTab.Screen
+            name='Profile'
+            component={Profile}
+            />
+        </AppTab.Navigator>
     )
 }
